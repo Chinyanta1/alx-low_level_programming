7e1c178f3d72468f9b9418cd90867a083787d242
chinyanta1 <chinyantamukumbwa@gmail.com>
2023-03-19 12:19:35 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,24 +10,19 @@
 int main(void)
 
 {
+	int i, j, k;
 
-	int d, p, q;
-
-	for (d = '0'; d <= '7'; d++)
+	for (i = 0; i <= 7; i++)
 	{
-
-	for (p = d + 1; p <= '8'; p++)
+	for (j = i + 1; j <= 8; j++)
 	{
-
-	for (q = p + 1; q <= '9'; q++)
+	for (k = j + 1; k <= 9; k++)
 	{
-	if ((p != d) != q)
+	putchar(i + '0');
+	putchar(j + '0');
+	putchar(k + '0');
+	if (i < 7 || j < 8 || k < 9)
 	{
-	putchar(d);
-	putchar(p);
-	putchar(q);
-	if (d == '/' && p == '8')
-	continue;
 	putchar(',');
 	putchar(' ');
 	}
